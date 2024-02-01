68b10b6e7ab9f2dc74b213e0d86bee27e3d22541
Gamer-Guy12 <123429926+Gamer-Guy12@users.noreply.github.com>
2024-01-31 22:09:02 -0800

    pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,7 +7,11 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"andy-bin","appId":"1:807535603849:web:79c817d016152ef7599658","storageBucket":"andy-bin.appspot.com","apiKey":"AIzaSyDv5C_30h5cel2p5f_bpYTycpECrk8ZfLo","authDomain":"andy-bin.firebaseapp.com","messagingSenderId":"807535603849","measurementId":"G-Z5Z74K51CD"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideAnalytics(() => getAnalytics())), ScreenTrackingService, UserTrackingService, importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideStorage(() => getStorage()))]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({ "projectId": "andy-bin", "appId": "1:807535603849:web:79c817d016152ef7599658", "storageBucket": "andy-bin.appspot.com", "apiKey": "AIzaSyDv5C_30h5cel2p5f_bpYTycpECrk8ZfLo", "authDomain": "andy-bin.firebaseapp.com", "messagingSenderId": "807535603849", "measurementId": "G-Z5Z74K51CD" }))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideAnalytics(() => getAnalytics())), ScreenTrackingService, UserTrackingService, importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideStorage(() => getStorage())), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })]
 };
