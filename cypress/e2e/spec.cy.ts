7ef7def2f5bc426c8541daeb048961c1fb9d2712
Gamer-Guy12 <123429926+Gamer-Guy12@users.noreply.github.com>
2024-01-31 22:05:33 -0800

    Added signin

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,5 @@
 describe('Initial Test', () => {
   it('Visits the initial project page', () => {
     cy.visit('/')
-    cy.contains('app is running!')
   })
 })
--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-signin',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './signin.component.scss'
 })
 export class SigninComponent {
+  email: string = ""
+  password: string = ""
+  auth = inject(Auth)
 
+  signIn() {
+    alert("click")
+    signInWithEmailAndPassword(this.auth, this.email, this.password)
+  }
 }
