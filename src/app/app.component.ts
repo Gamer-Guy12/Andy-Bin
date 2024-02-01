584fca46c875d1ec7ae033bf78f1c204c5e92f94
Gamer-Guy12 <123429926+Gamer-Guy12@users.noreply.github.com>
2024-01-31 21:10:19 -0800

    Replace stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopbarModule } from './topbar/topbar.module';
+import { UtilModule } from './util/util.module';
+import { AuthModule } from './auth/auth.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopbarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './util/not-found/not-found.component';
+import { SigninComponent } from './auth/signin/signin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "auth/signin",
+    component: SigninComponent,
+    title: "Andy Bin: Sign In"
+  },
+  {
+    path: "",
+    redirectTo: "auth/signin",
+    pathMatch: "full"
+  },
+  {
+    path: "**",
+    component: NotFoundComponent,
+    title: "Andy Bin: Not Found"
+  }
+];
